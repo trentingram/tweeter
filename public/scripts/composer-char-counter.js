f79f5b65de1cce973220125e12eab8202e9608a3
trentingram <tingram@homeaway.com>
2019-01-24 00:33:08 +0000

    adds .empty method to remove all child elements and  adds GET ajax request to render tweets on successful POST.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,55 +1,9 @@
+// fix alert when text is not present.
 // icons are broken.  Need to return to fix icons. Consider export/import file.
 // convert createdAt to days ago...
 // add margin or padding between tweets.
 // fix hover, hover should only work on individual tweets.
 // fix scroll issue.
-
-const tweetsData = [{
-    "user": {
-        "name": "Newton",
-        "avatars": {
-            "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-            "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-            "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-    },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-},
-{
-    "user": {
-        "name": "Descartes",
-        "avatars": {
-            "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-            "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-            "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd"
-    },
-    "content": {
-        "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-},
-{
-    "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-            "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-            "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-            "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-    },
-    "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-}
-];
 
 
 $(document).ready(function() {
@@ -74,20 +28,29 @@ $('.tweets').on('mouseleave', function(e) {
     $('.tweet-icons').toggle()
 });
 
+
 // listen for form submit
 $('.tweet-submit').on('submit', function(e) {
     e.preventDefault();
+    var theText = $('textarea[name=text]').val();
     let $serializedData = $(this).serialize();
-    console.log($serializedData);
-    // $myText = $('textarea[name=text]').val();
+    console.log($serializedData, 'theText: ',theText);
 
+    if(theText.length === 0) { 
+        alert('There is nothing to tweet about.') 
+    } else if(theText.length > 140) { 
+        alert('Your tweet is too verbose.')
+    } else {
     $.ajax('/tweets/', {
-    method: 'POST',
-    data: $serializedData,
-    success: function (data) {
-        console.log(data)
+        method: 'POST',
+        data: $serializedData,
+        success: function () {
+            $('.tweets').empty();
+            $.get('/tweets/')
+            .then(jsonData => renderTweets(jsonData))
+            console.log("post success")
+        }
+        })
     }
 })
-})
-
 });
