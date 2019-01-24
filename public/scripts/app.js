e0cc506fa4827be1841348222de45e9906955d14
trentingram <tingram@homeaway.com>
2019-01-24 00:53:45 +0000

    adds escape function to  escape text to  prevent XSS.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,24 +3,31 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+
+function escape(str) {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
+//   const safeHTML = `<p>${escape(textFromUser)}</p>`;
 function createTweetElement(data) {
     var $html =
         '<article class="article-tweets">' +
         '<header class="tweet-header">' +
         '<div class="tweet-image-name-container">' +
-        `<img class="tweet-image" src=${data.user.avatars.small}></img>` +
-        `<h2 class="tweet-name">${data.user.name}</h2>` +
+        `<img class="tweet-image" src=${escape(data.user.avatars.small)}></img>` +
+        `<h2 class="tweet-name">${escape(data.user.name)}</h2>` +
         '</div>' +
         '<div class="tweet-handle-container">' +
-        `<p class="handle">${data.user.handle}</p>` +
+        `<p class="handle">${escape(data.user.handle)}</p>` +
         '</div>' +
         '</header>' +
         '<section class="tweet-text">' +
-        `<p class="tweet-text-area">${data.content.text}</p>` +
+        `<p class="tweet-text-area">${escape(data.content.text)}</p>` +
         '</section>' +
         '<footer class="tweet-footer-container">' +
         '<div class="tweet-footer">' +
-        `<p class="tweet-footer-text">${data.created_at}</p>` +
+        `<p class="tweet-footer-text">${escape(data.created_at)}</p>` +
         '</div>'
     '<div class="tweet-icons">' +
     '<i class="fas fa-flag"></i>' +
@@ -43,10 +50,10 @@ function renderTweets(tweets) {
     })
 }
 
-// $(document).ready(function() {
-//     console.log('ready to fetch data')
-//     $.get('/tweets/')
-//     .then(jsonData => renderTweets(jsonData))
+$(document).ready(function() {
+    console.log('ready to fetch data')
+    $.get('/tweets/')
+    .then(jsonData => renderTweets(jsonData))
   
-// });
+});
 
